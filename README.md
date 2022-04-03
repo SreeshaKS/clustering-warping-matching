27a515f4a38f3aa7f9bf91e4d9df5277bb8dba78
Sreesha Kuruvadi <sreeshakuruvadi@Sreeshas-MacBook-Pro.local>
2022-04-03 16:54:53 -0400

    updated readme partially and implemneted orb with accuracy and FP , FN code

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-# a2
+# a2 - Part 1 - Image clustering using ORB
 
 ## General Procedure
 - Create descritors using ORB
 - Run sklearn.Aglomeratice clustering algorithm with the number of given clusters
 - used pairwise_distance function to calculate distance matrix
 - used the BFMatcher.knnMatch with k=2 and a ratio test with 0.75 threshold
-- sorted the matches which pass the threshold and picked the worst distance descriptor as a metric for distance matrix
+- metric used was merely the total number of matches
+- by using mean(distance of matches) / (total distance of all matches) - accuracy increases by 1 % from 81 - 82
+
+## A few observations
+- increasing the features from 1000 ( 81 % ) to 2000 ( 82 % ), increases accuracy 1%
+- using 'complete' linkage gives the higest accuracy and 'single' gives the lowest
+- Accuracy increases to 83.3 % from 81 %
+  - using complete linkage
+  - 'mean(distance of matches) / (total distance of all matches)'
+  - 2000 features ( 50 seconds TAT )
+  - 1000 features - gives the fastest results with a manageable dip in accuracy to 82.4 % ( 25 seconds TAT )
+
+### False Positives
+
+### False Negatives
--- a2.py
+++ a2.py
@@ -12,60 +12,77 @@ from sklearn import metrics
 import sklearn
 
 
+def metric(x, y):
+    x, y = np.float32(x), np.float32(y)
+    matches = cv2.BFMatcher().knnMatch(x, y, k=2)
+    good = []
+    good_dist = []
+    for m, n in matches:  # for each descriptor, consider closest two matches
+        if m.distance < 0.75 * n.distance:  # best match has to be 0.75 much closer than second best
+            good_dist.append(m.distance+0.00001)
+            good.append([m])
+
+    if len(good_dist) == 0:
+        return 0, good
+    # return len(good), good # - this gives an accuracy of 81.4%
+    return np.mean(good_dist) / sum(good_dist), good # - this gives an accuracy of 82.4%
+
+def prediction_accuracy(c_labels, lables, n, descs_list, images, kp_list):
+    lables = np.array([i.split('_')[0]
+                        for i in lables])  # retrieve names of files
+
+    tp = 0
+    tn = 0
+    t = n * (n - 1)  # total pairs
+
+    for i in range(len(c_labels)):
+        for j in range(len(c_labels)):
+            if i != j:  # do not compare the image with itself
+                m, good = metric(descs_list[i], descs_list[j])
+                out_img = np.zeros((200, 200, 3), np.uint8)
+                img3 = cv2.drawMatchesKnn(
+                    images[i], kp_list[i], images[j], kp_list[j], good, flags=2, outImg=out_img
+                )
+                # If image belong to the same cluster and has the same filename
+                if (c_labels[i] == c_labels[j]) & (lables[i] == lables[j]):
+                    tp += 1
+                # If image belongs to different class and has a separate file name
+                elif (c_labels[i] != c_labels[j]) & (lables[i] != lables[j]):
+                    tn += 1
+                # FP
+                # elif (c_labels[i] == c_labels[j]) & (lables[i] != lables[j]):
+                #     cv2.imwrite('results/fp-{}-{}.jpg'.format(lables[i], lables[j]), img3)
+                # FN
+                # elif (c_labels[i] != c_labels[j]) & (lables[i] == lables[j]):
+                #     cv2.imwrite('results/fn-{}-{}.jpg'.format(lables[i], lables[j]), img3)
+
+    return ((tp+tn)/t) * 100
+
 def part1(image_list, n_clusters):
     lables = [file.split('/')[-1] for file in image_list]
     images = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in image_list]
     n_images = len(images)
 
     descs_list = []
+    kp_list = []
+    
+    # read images and compute orb descriptors
     for i in images:
         totalFeaturePoints = 1000
         orb = cv2.ORB_create(nfeatures=totalFeaturePoints)
         kp, descriptors = orb.detectAndCompute(i, None)
+        kp_list.append(kp)
         descs_list.append(descriptors)
-
-    def metric(x, y):
-        x, y = np.float32(x), np.float32(y)
-        matches = cv2.BFMatcher().knnMatch(x, y, k=2)
-        good = []
-        for m, n in matches:  # for each descriptor, consider closest two matches
-            if m.distance < 0.75 * n.distance:  # best match has to be 0.75 much closer than second best
-                good.append(m.distance)
-        s = sorted(good)
-
-        if len(good) == 0:
-            return 0
-        return np.mean(good) / len(good)
-
-    def prediction_accuracy(c_labels, lables, n):
-        lables = np.array([i.split('_')[0] for i in lables])  # retrieve names of files
-
-        tp = 0
-        tn = 0
-        t = n*(n-1)  # total pairs
-
-        for i in range(len(c_labels)):
-            for j in range(len(c_labels)):
-                if i != j:  # do not compare the image with itself
-                    # If image belong to the same cluster and has the same filename
-                    if (c_labels[i] == c_labels[j]) & (lables[i] == lables[j]):
-                        tp += 1
-                    # If image belongs to different class and has a separate file name
-                    elif (c_labels[i] != c_labels[j]) & (lables[i] != lables[j]):
-                        tn += 1
-
-        return ((tp+tn)/t) * 100
 
     # Compute the distance matrix
     desc_distance = np.zeros(shape=(n_images, n_images))
     for idx, x in enumerate(descs_list):
         for idy, y in enumerate(descs_list):
-            desc_distance[idx][idy] = metric(x, y)
-
-    # desc_distance = pairwise_distances(descs_list, descs_list, metric)
+            desc_distance[idx][idy], _ = metric(x, y)
 
     affinityVal = 'precomputed'
-    linkageVal = 'complete' # complete gives the highest accuracy of 83 among ['complete', 'average', 'single']
+    # complete gives the highest accuracy of 83 among ['complete', 'average', 'single']
+    linkageVal = 'complete'
 
     ac = sklearn.cluster.AgglomerativeClustering(
         n_clusters=n_clusters, linkage=linkageVal, affinity=affinityVal
@@ -75,7 +92,9 @@ def part1(image_list, n_clusters):
     for idx, p in enumerate(ac.labels_):
         lines[p] += lables[idx] + ' '
 
-    return '\n'.join(lines), prediction_accuracy(ac.labels_, lables, n_images)
+    return '\n'.join(lines), prediction_accuracy(
+        ac.labels_, lables, n_images, descs_list, images, kp_list
+        )
 
 
 if sys.argv[1] == 'part1':
